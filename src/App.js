f49eae98710d85ce90398596f1e1860eee6505b5
Martynas Majaras <martynasmajaras98@gmail.com>
2021-11-03 12:47:12 +0100

    Created components for different kinds of pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route  } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from './components/HomePage';
+import AllProductsPage from './components/AllProductsPage';
+import ProductPage from './components/ProductPage';
+import DetailsPage from './components/DetailsPage';
+
+class App extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      AllProducts: [],
+      hasError: false
+    }
+  }
+ 
+  getProducts = () => {
+    axios.get('')
+    .then((response) => {
+        this.setState( {AllProducts: response.data})
+     })  
+     .catch((error) => {
+      this.setState({
+          hasError: true
+      })
+    })           
+  }
+
+  componentDidMount() {
+    this.getProducts();
+ }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <div className='container'>
+                <h1>Start Page</h1>
+                <Link to='/'><h4>Home</h4></Link>
+                <Link to='/ProductList'><h4>Product</h4></Link>
+            </div>
+          </header>
+          <main>
+             <Route path='/' exact render={ () =>  <HomePage/> } />
+             <Route path='/AllProductsPage' render= { () => <AllProductsPage AllProducts={this.state.AllProducts}/>} />
+             <Route path='/ProductPage/:id' render = { (props) => <ProductPage  id={props.match.params.id}/>  } />
+             <Route path='/DetailsPage/:id' render = { (props) => <DetailsPage id={props.match.params.id} />} />
+         </main>
+        </div>
+      </Router>
+    );
+  }
 }
-
+  
 export default App;
---
+++ src/components/AllProductsPage.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class AllProductsPage extends Component  {
+    constructor(props) {
+      super(props);
+      this.state = {
+    
+      }
+    }
+    render() {
+      return (
+          <div className="App">
+            <header className="App-header">
+             
+            </header>
+          </div>
+      );
+    }
+  }
+    
+  export default AllProductsPage;
